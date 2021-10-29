529ae0a4050ce2c9811939490d8683250a535cd5
Nguyễn Huy Hoàng <81377121+HUYHoanG-c@users.noreply.github.com>
2021-10-29 11:12:36 +0700

    https://github.com/LapTrinhHDT/Ho-ng.git

--- New1/bai1.cpp
+++ New1/bai1.cpp
@@ -1,6 +1,62 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<< "lam bai tap lon"<<endl;
-    return 0;
+
+class DayOfBirth{
+	protected:
+		int day;
+		int month;
+		int year;
+	public:
+		DayOfBirth();
+		DayOfBirth(int day, int month, int year);
+		DayOfBirth(const DayOfBirth &ngay);
+		~DayOfBirth();
+		void NhapNgay();
+		void XuatNgay();
+};
+DayOfBirth::DayOfBirth(){
+	day=0;
+	month=0;
+	year=0;
 }
+DayOfBirth::DayOfBirth(int day, int month, int year){
+	day = day;
+	month = month;
+	year = year
+}
+DayOfBirth::DayOfBirth(const DayOfBirth &ngay){
+	*this = ngay;
+}
+DayOfBirth::~DayOfBirth(){
+}
+void DayOfBirth::NhapNgay(){
+    char PhanChia;   //phan chia dung ki tu "/"
+    cout<<"\nnhap ngay, thang, nam sinh(dd/mm/yyyy)  ";
+    cin>>day>>PhanChia>>month>>PhanChia>>year;
+}
+void DayOfBirth::XuatNgay(){
+	cout<<ngay<<"/"<<thang<<"/"<<nam<<endl;
+}
+
+
+class Nguoi : public DayOfBirth{
+	private:	
+		string Name;
+		int age;
+		string sex;
+		DayOfBirth ns;
+		
+		
+		
+	public:
+			 
+}; 
+class NhanVien{
+	
+};
+class ThuKi{
+	
+};
+class CapQuanTri{
+	
+};
